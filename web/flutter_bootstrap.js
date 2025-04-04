c93c15adb3352aefa7045280adea1ef7a6ae599c
Eluvk <eluvk.dev@gmail.com>
2025-04-04 12:23:21 +0800

    bootstrap && 404

---
+++ web/flutter_bootstrap.js
@@ -0,0 +1,17 @@
+{{flutter_js}}
+{{flutter_build_config}}
+
+const loading = document.querySelector('#loading');
+_flutter.loader.load({
+  onEntrypointLoaded: async function (engineInitializer) {
+    loading.classList.add('main_done');
+    const appRunner = await engineInitializer.initializeEngine();
+
+    loading.classList.add('init_done');
+    await appRunner.runApp();
+
+    window.setTimeout(function () {
+      loading.remove();
+    }, 200);
+  }
+});
